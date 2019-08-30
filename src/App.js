493b9bec178068721fee657f63ab621f7ad5609d
maldoalex <email@address.com>
2019-08-30 13:45:07 -0500

    started Dashboard component and profile component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
+import routes from "./routes";
+import Nav from "./components/Nav/Nav";
+
 import "./App.css";
 
-function App() {
-  return <div className="App">KeepItReel</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App">
+          <Nav />
+          {routes}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
