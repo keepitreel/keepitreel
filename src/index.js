96ac04dda0555d9b495f306b455ad38772e978ec
Philip Schultz <24783715+schupp13@users.noreply.github.com>
2019-09-02 16:07:41 -0500

    added the store to the provider

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
 
+import store from "./store";
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
