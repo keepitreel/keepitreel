5ac655484154ba1c741cc6cbc5ce5a9e2689e098
maldoalex <email@address.com>
2019-09-02 23:15:54 -0500

    completed authreducer for login and register

--- src/redux/movieReducer.js
+++ src/redux/movieReducer.js
@@ -1,41 +1,53 @@
-import axios from axios;
+import axios from "axios";
 
-const initialState= {
+const initialState = {
   movies: [],
   movie: "",
   error: "",
   totalResults: 0,
   buttons: 0,
   pagination: [],
-  displayButtons: "" 
-}
-
+  displayButtons: ""
+};
 
 const FETCH_MOVIES = "FETCH_MOVIES";
 
-
-export function getMovies(){
+export function getMovies() {
   return {
     type: FETCH_MOVIES,
     payload: axios
-    .get(
-      `http://www.omdbapi.com/?s=${this.state.movie}&page=1&apikey=579b4fff`
-    )
-    .then(response => {
-      response.data
-         }).catch(error = console.log(error))
-  }}
+      .get(
+        `http://www.omdbapi.com/?s=${this.state.movie}&page=1&apikey=579b4fff`
+      )
+      .then(response => {
+        return response.data;
+      })
+    // .catch((error = console.log(error)))
+  };
+}
 
+export default function movieReducer(state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case `${FETCH_MOVIES}_PENDING`:
+      return { ...state, loading: true };
 
-  export function movieReducer(state = initialState, action) {
-    const {type, payload} = action;
-    switch(type){
-     case `${FETCH_MOVIES}_PENDING`:
-       return {...state, loading: true}
-    
-     case `${FETCH_MOVIES}_FULFILLED`:
-      return  payload.Data.Response === "True" ? 
-       {...state, loading:false, movies: payload.data.Search, totalResults: payload.data.Number(payload.data.totalResults), buttons: Math.ceil(Number(payload.data.totalResults) / 10) }:
-       {movies: [], error: payload.data.Error, pagination:[], displayButtons:""}
-    }
+    case `${FETCH_MOVIES}_FULFILLED`:
+      return payload.Data.Response === "True"
+        ? {
+            ...state,
+            loading: false,
+            movies: payload.data.Search,
+            totalResults: payload.data.Number(payload.data.totalResults),
+            buttons: Math.ceil(Number(payload.data.totalResults) / 10)
+          }
+        : {
+            movies: [],
+            error: payload.data.Error,
+            pagination: [],
+            displayButtons: ""
+          };
+    default:
+      return state;
   }
+}
