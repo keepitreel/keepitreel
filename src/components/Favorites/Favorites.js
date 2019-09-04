a9f5c650c761e7fb388130e653db36267bba5584
maldoalex <email@address.com>
2019-09-04 15:32:45 -0500

    completed Card comp, connected the back end to the dashboard tabs

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
+import Card from "../Card/Card";
+import { connect } from "react-redux";
+import axios from "axios";
 
 class Favorites extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      favorites: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`/api/favoritepost/${this.props.user_id}`).then(res => {
+      this.setState({
+        favorites: res.data
+      });
+      console.log(this.state);
+    });
   }
   render() {
-    return <div className="favorites-wrapper">Favorites</div>;
+    return (
+      <>
+        <div className="friends-container">
+          <h1>Favorites</h1>
+          <div>
+            {this.state.favorites.map(favorite => (
+              <Card
+                name={favorite.name}
+                post_id={favorite.post_id}
+                key={favorite.post_id}
+                user_id={favorite.user_id}
+                text={favorite.text}
+                posterurl={favorite.posterurl}
+                title={favorite.title}
+                blogtitle={favorite.blogtitle}
+                avatarurl={favorite.avatarurl}
+                rating={favorite.rating}
+              />
+            ))}
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
-export default Favorites;
+let mapStatetoProps = reduxState => {
+  return {
+    user_id: reduxState.authReducer.user_id
+  };
+};
+
+export default connect(mapStatetoProps)(Favorites);
