438d1ab6792fc158dbffa3ff18175ea687c1e31b
Philip Schultz <24783715+schupp13@users.noreply.github.com>
2019-09-06 12:29:00 -0500

    liked should work

--- src/components/Like/Like.js
+++ src/components/Like/Like.js
@@ -8,56 +8,114 @@ class Like extends Component {
     super();
     this.state = {
       like: false,
+      dislike: false,
       likeClass: "",
-      unlikeClass: ""
+      dislikeClass: ""
     };
   }
   like = () => {
-    console.log(this.state.like);
     let { user_id, post_id } = this.props;
+    console.log(this.state.like);
+    this.state.like
+      ? this.setState({
+          like: !this.state.like,
+          likeClass: "",
+          dislikeClass: "",
+          dislike: false
+        })
+      : this.setState({
+          like: !this.state.like,
+          likeClass: "likeActive",
+          dislikeClass: "",
+          dislike: false
+        });
     axios
       .put("/api/viewcard/thumbsup", { user_id, post_id })
       .then(res => {
         console.log(res);
       })
       .catch(error => console.log(error));
-    this.setState({
-      like: !this.state.like,
-      likeClass: "likeActive",
-      dislikeActive: ""
-    });
   };
 
   unlike = () => {
-    console.log(this.state.like);
+    console.log(this.state.dislike);
     let { user_id, post_id } = this.props;
+    this.state.dislike
+      ? this.setState({
+          dislike: !this.state.dislike,
+          likeClass: "",
+          dislikeClass: "",
+          like: false
+        })
+      : this.setState({
+          dislike: !this.state.dislike,
+          dislikeClass: "dislikeActive",
+          likeClass: "",
+          like: false
+        });
     axios
       .put("/api/viewcard/thumbsdown", { user_id, post_id })
       .then(res => {
         console.log(res);
       })
       .catch(error => console.log(error));
-
-    this.setState({
-      like: !this.state.like,
-      likeClass: "",
-      dislikeActive: "dislikeActive"
-    });
   };
+
+  componentDidMount() {
+    let { user_id, post_id } = this.props;
+    axios
+      .put("/api/viewcard/liked", { user_id, post_id })
+      .then(res => {
+        console.log(res);
+        res.data
+          ? this.setState({
+              like: true,
+              likeClass: "likeActive",
+              dislike: false,
+              dislikeClass: ""
+            })
+          : this.setState({
+              like: false,
+              likeClass: "",
+
+              dislikeClass: ""
+            });
+
+        axios
+          .put("/api/viewcard/disliked", { user_id, post_id })
+          .then(respond => {
+            console.log(respond);
+            respond.data
+              ? this.setState({
+                  dislike: true,
+                  like: false,
+                  dislikeClass: "dislikeActive",
+                  likeClass: ""
+                })
+              : this.setState({
+                  dislike: false,
+                  dislikeClass: ""
+                });
+          });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
     console.log(this.state);
     return (
       <div className="LikeComponent">
         <div className={"tooltip"}>
           <i
-            class={`fas fa-thumbs-up ${this.likeClass}`}
+            class={`fas fa-thumbs-up ${this.state.likeClass}`}
             onClick={this.like}
           ></i>
           <span class="tooltiptext">Like Post</span>
         </div>
         <div className="tooltip">
           <i
-            class={`fas fa-thumbs-down tooltip ${this.unlikeClass}`}
+            class={`fas fa-thumbs-down tooltip ${this.state.dislikeClass}`}
             onClick={this.unlike}
           ></i>
           <span class="tooltiptext">Dislike Post</span>
