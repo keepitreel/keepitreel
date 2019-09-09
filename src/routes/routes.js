3603946e68a9db31bea05f7b98350841e20a824d
Ismail Gaddipati <Ismail@IMAC.Lobby>
2019-09-09 09:07:04 -0500

    added flip card functions/ completed star setup

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import SearchMovie from "../components/SearchMovie/SearchMovie";
 import CreateBlog from "../components/CreateBlog/CreateBlog";
-import LandingPageLogin from "../components/LandingPage-login/Landing-login.component.js";
+import LandingPageLogin from "../components/LandingPage-login/Landing-login.component";
 import Dashboard from "../components/Dashboard/Dashboard";
 import Blog from "../components/Blog/Blog";
 import UserPage from "../components/UserPage/UserPage";
