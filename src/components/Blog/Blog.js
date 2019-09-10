743dcb0870010b0958d869e14e37cf0923b0a205
Philip Schultz <24783715+schupp13@users.noreply.github.com>
2019-09-10 01:10:05 -0500

    I have know clue why Display  movie will not work in BlogForm.js

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -28,40 +28,6 @@ class Blog extends Component {
     });
   };
 
-  editPost = updatePost => {
-    let { post } = this.state;
-    let post_id = post[0].post_id;
-    let user_id = post[0].user_id;
-    let imdbid = post[0].imbdid;
-    let posterurl = post[0].posterurl;
-    let time = post[0].time;
-    let genre = post[0].genre;
-    let title = post[0].title;
-
-    let rating = this.state.editrating;
-    let blogtitle = this.state.editTitle;
-    let text = this.state.editText;
-    axios
-      .put("/api/dashboard/update/post", {
-        post_id,
-        user_id,
-        text,
-        imdbid,
-        posterurl,
-        rating,
-        time,
-        genre,
-        title,
-        blogtitle
-      })
-      .then(response => {
-        this.setState({ post: response.data });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-
   componentDidMount() {
     axios
       .get(`/api/dashboard/getpost/${this.props.match.params.id}`)
@@ -74,7 +40,7 @@ class Blog extends Component {
   }
   render() {
     let { post } = this.state;
-
+    console.log(post);
     let blogStuff = post.map(blog => {
       return (
         <div className="BlogContainer">
@@ -94,7 +60,9 @@ class Blog extends Component {
                 </Link>
                 {this.props.user_id == blog.user_id ? (
                   <div className="features">
-                    <i class="fas fa-edit"></i>
+                    <Link to={`/editblog/${blog.post_id}`}>
+                      <i class="fas fa-edit"></i>
+                    </Link>
 
                     <DeletePost post_id={blog.post_id} />
                   </div>
@@ -103,9 +71,8 @@ class Blog extends Component {
                     <Like post_id={blog.post_id} post_user_id={blog.user_id} />
                   </div>
                 ) : null}
-
                 <StarRating rating={blog.rating} />
-                <p>{blog.text}</p>
+                <p>{blog.text}</p>={" "}
               </div>
             </div>
             <div className="bottom">
@@ -113,7 +80,6 @@ class Blog extends Component {
                 <Comments post_id={this.props.match.params.id} />
               </div>
             </div>
-            <h1>test</h1>
           </div>
         </div>
       );
--- src/components/BlogForm/BlogForm.js
+++ src/components/BlogForm/BlogForm.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import Rating from "../Star/Rating";
-
+import DisplayMovie from "..//DisplayMovie/DisplayMovie";
+import axios from "axios";
+import "./BlogForm.scss";
 // this component should take in five props
 
 export default class BlogForm extends Component {
   constructor() {
     super();
     this.state = {
-      blogtitle: "",
+      post_id: "",
+      user_id: "",
       text: "",
-      rating: 0
+      imdbid: "",
+      posterurl: "",
+      rating: "",
+      time: "",
+      genre: "",
+      title: "",
+      blogtitle: ""
     };
   }
 
@@ -20,37 +29,100 @@ export default class BlogForm extends Component {
   };
 
   updateRating = value => {
+    console.log(value);
     this.setState({ rating: value });
   };
 
+  editPost = () => {
+    let {
+      post_id,
+      user_id,
+      text,
+      imdbid,
+      posterurl,
+      rating,
+      time,
+      genre,
+      title,
+      blogtitle
+    } = this.state;
+    axios
+      .put("/api/dashboard/update/post", {
+        post_id,
+        user_id,
+        text,
+        imdbid,
+        posterurl,
+        rating,
+        time,
+        genre,
+        title,
+        blogtitle
+      })
+      .then(response => {
+        this.setState({ post: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  componentDidMount() {
+    let updateRating = this.updateRating;
+    axios
+      .get(`/api/dashboard/getpost/${this.props.match.params.id}`)
+      .then(response => {
+        this.setState({
+          post: response.data,
+          blogtitle: response.data[0].blogtitle,
+          text: response.data[0].text,
+          post_id: response.data[0].post_id,
+          user_id: response.data[0].user_id,
+          imdbid: response.data[0].imdbid,
+          posterurl: response.data[0].posterurl,
+          rating: response.data[0].rating,
+          time: response.data[0].time,
+          genre: response.data[0].genre,
+          title: response.data[0].title
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      }); //gets single post given post_id
+  }
+
   render() {
-    let { post } = this.props;
-    let form = post.map(blog => {
-      return (
-        <form>
-          <label>
-            Title
-            <input
-              value={blog.blogtitle}
-              required
-              name="blogtitle"
-              onChange={this.handleChange}
-              autoFocus
-            ></input>
-          </label>
-          <label>
-            Blog
-            <textarea
-              value={blog.text}
-              name="text"
-              onChange={this.handleChange}
-            ></textarea>
-          </label>
-          <Rating updateRating={this.updateRating} />
-          <button onClick={this.handleSubmit}>Submit</button>
-        </form>
-      );
-    });
-    return <div className={"formDiv"}>{form}</div>;
+    console.log(this.state);
+
+    return (
+      <div className={"formDiv"}>
+        <div className="movieContainer">
+          <DisplayMovie ImdbID={this.state.imdbid} />
+        </div>
+        <div className="EditBlogPage">
+          <form>
+            <label>
+              Title
+              <input
+                value={this.state.blogtitle}
+                required
+                name="blogtitle"
+                onChange={this.handleChange}
+                autoFocus
+              ></input>
+            </label>
+            <label>
+              Blog
+              <textarea
+                value={this.state.text}
+                name="text"
+                onChange={this.handleChange}
+              ></textarea>
+            </label>
+            <Rating updateRating={this.updateRating} />
+            <button onClick={this.handleSubmit}>Submit</button>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
